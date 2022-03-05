d74e38cfa536099ec68477d826bc27c9e7fb3d8c
Raullbotz <100943424+Raullbotz@users.noreply.github.com>
2022-03-05 17:00:19 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,7 +8,7 @@ const chats = conn.chats.all()
 const groups = chats.filter(v => v.jid.endsWith('g.us'))
 const defaultMenu = {
   before: `
-║╭──❉ 〔 LynXzy 〕❉──
+║.╭──❉ 〔 LynXzy 〕❉──
 ║│➸ Hai, %name!
 ║│➸ Tersisa *%limit Limit*
 ║│➸ Role *%role*
